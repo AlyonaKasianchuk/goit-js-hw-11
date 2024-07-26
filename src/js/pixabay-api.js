a3dd4a9217a4da8de86c45c478048f32b7dee6f9
Alona Kasianchuk <136174868+AlyonaKasianchuk@users.noreply.github.com>
2024-07-26 16:53:26 +0300

    hw-11

---
+++ src/js/pixabay-api.js
@@ -0,0 +1,26 @@
+
+const API_KEY = '45127620-5b07992107ac4e8771e67df86'; 
+const BASE_URL = 'https://pixabay.com/api';
+
+
+export function fetchImages(query) {
+  const url = `${BASE_URL}/?key=${API_KEY}&q=${encodeURIComponent(query)}&image_type=photo&orientation=horizontal&safesearch=true`;
+
+  return fetch(url)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(response.status);
+      }
+      return response.json();
+    })
+    .then(data => {
+      if (data.hits.length === 0) {
+        throw new Error('No images found');
+      }
+      return data;
+    })
+    .catch(error => {
+      console.error('Error fetching images:', error);
+      throw error;
+    });
+}
